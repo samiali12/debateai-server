da2f036bb0648a91f9c1929c3fb6fc4f241070a4
Sami Ali <sami_jameel@nextpack.org>
2025-10-08 21:43:44 +0500

    generate requirements.txt

---
+++ src/database/session.py
@@ -0,0 +1,22 @@
+import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+session = sessionmaker(autoflush=False, bind=engine)
+base = declarative_base()
+
+print(engine)
