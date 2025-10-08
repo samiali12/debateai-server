4ffce8bccefe1ecdcc649f3a57a1c5bcd4736ac9
Sami Ali <sami_jameel@nextpack.org>
2025-10-08 22:21:29 +0500

    setup logger and database connection

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database.session import engine
+from utils.logger import logger
 
-app = FastAPI()
 
-@app.get('/')
-async def home():
-    return "Hello I am ready"
+
+@asynccontextmanager
+async def startup_event(app:FastAPI):
+    try:
+        with engine.connect() as conn:
+            logger.info('✅ Database connected successfully!')
+        yield
+    except Exception as e:
+        logger.error(f'❌ Database connection failed: {e}')
+
+
+app = FastAPI(lifespan=startup_event)
--- src/database/session.py
+++ src/database/session.py
@@ -18,5 +18,3 @@ DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB
 engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(autoflush=False, bind=engine)
 base = declarative_base()
-
-print(engine)
