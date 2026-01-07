f6ae1edeab72eae7b6bb4fbe13b878632ba33b25
Sami Ali <samijameel100@gmail.com>
2026-01-07 23:13:03 +0500

    feat: remove redis

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -1,15 +1,10 @@
 from fastapi import HTTPException, status
 from core.security import verify_token
-from database.redis import redis_client
 from fastapi import Request
 
 
 def is_authenticated(request: Request):
     token = request.cookies.get("access_token")
-    if redis_client.get(f"blacklist:{token}"):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token revoked"
-        )
 
     if not isinstance(token, str):
         raise HTTPException(
